1c90c77016e98e9a53cfd2e04bbee1bd0b8b3dff
shashikala <chandrasekaremsmh@gmail.com>
2023-08-13 18:03:16 +0530

    Completed Algo-3

--- Algo3.java
+++ Algo3.java
@@ -4,7 +4,27 @@ public class Algo3 {
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         int[] nums = {10, 20, 30, 40, 50};
+        int[] numsNew = new int[5];
+
+        for (int i = 4; i > -1; i--) {
+            int index = 0;
+            index = nums[i];
+            numsNew[4-i] = index;
+               
+            
+            
         
+            
+        }
+        nums = numsNew;
+        System.out.print("Nums = [");
+        for (int k = 0; k < nums.length; k++) {
+            System.out.print(nums[k]+",");
+          
+        }
+        System.out.println("\b"+ "]");  
+        // System.out.println(nums);
+        // System.out.println(numsNew);
 
     }
     
