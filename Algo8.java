7cdf5fb208a298d8c8cfc701bd583187f756753b
shashikala <chandrasekaremsmh@gmail.com>
2023-08-14 09:19:10 +0530

    Completed Algo-8

---
+++ Algo8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Algo8 {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] numA = {5, 7, -2, 3, 4, 6, 7};
+        int[] numB = {7, 8, -8, 2, 1, -9, 6};
+
+        int[] intersect = new int[];
+        for (int i = 0; i < numB.length; i++) {
+            
+        }
+        
+    }
+    
+}
