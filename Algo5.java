22e64634020e2022c6826db4df31895162da64b9
shashikala <chandrasekaremsmh@gmail.com>
2023-08-14 10:25:55 +0530

    Modified completed Algo-5

--- Algo5.java
+++ Algo5.java
@@ -4,15 +4,24 @@ public class Algo5 {
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         
-
+        loop:
         do {
             System.out.print("Enter a text: ");
-            String text = scanner.nextLine();
+            String text = scanner.nextLine().strip();
             char[] textStr = text.toCharArray();
             if (text.isEmpty()) {
-                System.out.println("Please enter a text");
+                System.out.println("Text can't be empty");
+                continue;
                 
             }
+            for (int i = 0; i < textStr.length; i++) {
+                if (textStr[i]>128) {
+                    System.out.println("Only ASCII characters are supported");
+                    continue loop;
+                }
+            }
+            
+
             int a = text.length()-1;
             char[] invert = new char[text.length()];
             for (int i = (text.length() - 1); i > -1; i--) {
@@ -31,6 +40,7 @@ public class Algo5 {
         
 
 
+            break;
 
         }while(true);
         
